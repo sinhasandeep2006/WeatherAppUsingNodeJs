fe197ad634a5f476b7129dd2a9a2041b74aa1fd2
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-07-23 15:55:09 +0530

    home page is ready

--- public/js/script.js
+++ public/js/script.js
@@ -53,3 +53,50 @@ document.addEventListener('DOMContentLoaded', function() {
         this.innerHTML = type === 'password' ? '<i class="fas fa-eye"></i>' : '<i class="fas fa-eye-slash"></i>';
     });
 });
+
+const apikey = "65ec89d04d3d00c44c849c374bd157c1";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city, elementId) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+    const element = document.querySelector(`#${elementId}`);
+    
+    if (response.status === 404) {
+        element.style.display = "none";
+    } else {
+        const data = await response.json();
+        element.getElementsByClassName("city")[0].innerHTML = data.name;
+        element.getElementsByClassName("temp")[0].innerHTML = Math.round(data.main.temp) + "°C";
+        const weatherIcon = element.getElementsByClassName("weather-icon")[0];
+
+        switch (data.weather[0].main) {
+            case "Clouds":
+                weatherIcon.src = "/img/clouds.png";
+                break;
+            case "Clear":
+                weatherIcon.src = "/img/clear.png";
+                break;
+            case "Drizzle":
+                weatherIcon.src = "/img/drizzle.png";
+                break;
+            case "Mist":
+                weatherIcon.src = "/img/mist.png";
+                break;
+            case "Rain":
+                weatherIcon.src = "/img/rain.png";
+                break;
+            case "Snow":
+                weatherIcon.src = "/img/snow.png";
+                break;
+        }
+        element.style.display = "block";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    checkWeather("New Delhi", "weather-new-delhi");
+    checkWeather("USA", "weather-usa");
+    checkWeather("Tokyo", "weather-tokyo");
+    checkWeather("Russia", "weather-russia");
+});
+
